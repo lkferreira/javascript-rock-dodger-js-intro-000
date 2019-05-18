d29d3826511c7d31a9c4425fe61d2d88473e6220
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-18 03:05:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if (e.which === LEFT_ARROW) {
+   if (e.which == LEFT_ARROW) {
     moveDodgerLeft()
   }
 
